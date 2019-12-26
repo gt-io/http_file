9e4c0cd1c09637ecb90661d976efea2fc8a790cb
jaksal <jaksal@gmail.com>
2019-12-26 22:04:42 +0900

    add watch

--- client/main.go
+++ client/main.go
@@ -10,7 +10,11 @@ import (
 	"os"
 	"path/filepath"
 	"time"
+
+	"github.com/fsnotify/fsnotify"
 )
+
+var url string
 
 func main() {
 	// open file db
@@ -19,37 +23,108 @@ func main() {
 		panic(err)
 	}
 
-	var url, path string
+	var path string
 
 	flag.StringVar(&url, "url", "http://localhost:8080/upload", "upload path")
-	flag.StringVar(&path, "path", "", "upload file")
+	flag.StringVar(&path, "path", "", "watch path")
 
 	flag.Parse()
 
-	// check file exist
-	if info, err := os.Stat(path); os.IsNotExist(err) || info.IsDir() {
-		log.Fatalln("file not exist", path, os.IsNotExist(err))
-		return
-	}
-
-	// check aleady uploaded
-	if exist, _ := ExistData(path); exist {
-		log.Println("aleady exist data", path)
-		return
-	}
-
-	log.Println("upload start ", url, path)
-
-	if err := upload(url, path); err != nil {
-		log.Println("file upload error")
-		return
-	}
-
-	AddData(path, "", time.Now())
-
+	checkExistFile(path)
+	watch(path)
 }
 
-func upload(url, path string) error {
+func checkExistFile(path string) error {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		// check aleady uploaded
+		if exist, _ := ExistData(file.Name()); exist {
+			log.Println("aleady exist data", file.Name())
+			continue
+		}
+
+		log.Println("upload start ", file.Name())
+
+		if err := upload(file.Name()); err != nil {
+			log.Println("file upload error", err, file.Name())
+			continue
+		}
+
+		AddData(file.Name(), "", time.Now())
+	}
+	return nil
+}
+
+func watch(path string) error {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				log.Println("event:", event)
+				if event.Op&fsnotify.Create == fsnotify.Create {
+					log.Println("create file:", event.Name)
+					fn := event.Name
+
+					// check file exist
+					if info, err := os.Stat(fn); os.IsNotExist(err) || info.IsDir() {
+						log.Fatalln("file not exist", fn, os.IsNotExist(err))
+						return
+					}
+
+					// check aleady uploaded
+					if exist, _ := ExistData(fn); exist {
+						log.Println("aleady exist data", fn)
+						return
+					}
+
+					log.Println("upload start ", fn)
+
+					if err := upload(fn); err != nil {
+						log.Println("file upload error")
+						return
+					}
+
+					AddData(fn, "", time.Now())
+
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("watch start folder :", path)
+	<-done
+
+	return nil
+}
+
+func upload(path string) error {
 	r, w := io.Pipe()
 	m := multipart.NewWriter(w)
 	go func() {
@@ -60,7 +135,7 @@ func upload(url, path string) error {
 			log.Println("multipart CreateFormFile error", err)
 			return
 		}
-		log.Println("file open start",path)
+		log.Println("file open start", path)
 		file, err := os.Open(path)
 		if err != nil {
 			log.Println("upload file os.Open error", err, path)
@@ -68,14 +143,13 @@ func upload(url, path string) error {
 		}
 		defer file.Close()
 
-		log.Println("file copy start",path)
+		log.Println("file copy start", path)
 		if _, err = io.Copy(part, file); err != nil {
 			log.Println("io.Copy error", err)
 			return
 		}
-		log.Println("file copy finish",path)
+		log.Println("file copy finish", path)
 	}()
-
 
 	resp, err := http.Post(url, m.FormDataContentType(), r)
 	if err != nil {
